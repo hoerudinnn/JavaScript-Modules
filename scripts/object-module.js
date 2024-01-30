d97df82ad66607dc3e46dbd610313e903765b888
Hoerudin <hoerudinpermana30@gmail.com>
2024-01-30 23:56:39 +0700

    object module

---
+++ scripts/object-module.js
@@ -0,0 +1,13 @@
+export const firstName = 'John';
+export const middleName = 'Tuck'
+export const lastName = 'Doe';
+
+export function say() {
+    console.info(`Hello from Hoer`);
+}
+
+export class Person {
+    constructor() {
+        this.name = "Hoer"
+    }
+}
