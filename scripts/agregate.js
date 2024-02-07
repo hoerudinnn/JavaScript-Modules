ce818b4bdc9eca67c00f4629bc714255cd1d7499
Hoerudin <hoerudinpermana30@gmail.com>
2024-02-07 22:05:39 +0700

    agregate

---
+++ scripts/agregate.js
@@ -0,0 +1,8 @@
+// Ekspor fungsi sayHello dan sayGoodBye dari say.js
+export { sayHello, sayGoodBye } from "./say.js";
+
+// Ekspor kelas Person dari class.js
+export { Person } from "./class.js";
+
+// Ekspor fungsi sum dari multiple.js
+export { sum } from "./multiple.js";
